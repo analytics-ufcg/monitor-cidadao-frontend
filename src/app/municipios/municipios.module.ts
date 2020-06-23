e80cdef97251579c3edfcea41fadd1615678c00d
Joaquim Honório <joaquimhonorio@copin.ufcg.edu.br>
2020-06-23 08:10:41 -0300

    Adiciona listagem de contratos de um município default

--- src/app/municipios/municipios.module.ts
+++ src/app/municipios/municipios.module.ts
@@ -1,19 +1,23 @@
+import { PipesModule } from './../shared/pipes/pipes.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MunicipiosLicitacoesComponent } from './municipios-licitacoes/municipios-licitacoes.component';
 import { MunicipiosComponent } from './municipios.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { MunicipioRoutingModule } from './municipios-routing.module';
+import { ListaContratosComponent } from './lista-contratos/lista-contratos.component';
 
 @NgModule({
   declarations: [
     MunicipiosLicitacoesComponent,
-    MunicipiosComponent
+    MunicipiosComponent,
+    ListaContratosComponent
   ],
   imports: [
     CommonModule,
     NgxPaginationModule,
     MunicipioRoutingModule,
+    PipesModule
   ],
   exports:[
     MunicipiosComponent,
