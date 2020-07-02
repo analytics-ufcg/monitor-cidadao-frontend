e693732d0a2c53b0875e08223ac58b8aa0153962
diegooalmeida <diego.almeida@ccc.ufcg.edu.br>
2020-07-02 12:01:42 -0300

    corrige/apaga componentes não mais utilizados nos relatorios

--- src/app/relatorios/relatorios-routing.module.ts
+++ src/app/relatorios/relatorios-routing.module.ts
@@ -1,9 +1,6 @@
 import { ListaRelatoriosComponent } from './lista-relatorios/lista-relatorios.component';
-import { RelatorioComponent } from './relatorio/relatorio.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
 
 
 const routes: Routes = [
@@ -12,10 +9,7 @@ const routes: Routes = [
     path: '',
     component: ListaRelatoriosComponent 
   },
-  {
-    path: ':id',
-    component: RelatorioComponent
-  }
+  
 ];
 
 @NgModule({
