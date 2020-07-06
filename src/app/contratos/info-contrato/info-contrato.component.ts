333e8c4411a9e1d9501509333ad83581b743a954
biabs1 <beatriz.souza@ccc.ufcg.edu.br>
2020-07-06 12:09:01 -0300

    Altera tela de contrato

--- src/app/contratos/info-contrato/info-contrato.component.ts
+++ src/app/contratos/info-contrato/info-contrato.component.ts
@@ -1,4 +1,8 @@
+import { debounceTime, takeUntil } from 'rxjs/operators';
 import { Component, OnInit, Input } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Municipio } from './../../shared/models/municipio.model';
+import { UserService } from './../../shared/services/user.service';
 
 @Component({
   selector: 'info-contrato',
@@ -9,9 +13,24 @@ export class InfoContratoComponent implements OnInit {
 
   @Input() contrato;
 
-  constructor() { }
+  private unsubscribe = new Subject();
+  public municipioEscolhido: Municipio;
+
+  constructor(private userService: UserService) { }
 
   ngOnInit(): void {
+    this.getMunicipio();
+  }
+
+  getMunicipio() {
+    this.userService
+      .getMunicipioEscolhido()
+      .pipe(
+        debounceTime(300),
+        takeUntil(this.unsubscribe))
+      .subscribe(municipio => {
+        this.municipioEscolhido = municipio;
+      });
   }
 
 }
