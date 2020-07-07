3cdb94e58bfc498126b1cd4d8adee2b58b3eaa34
Joaquim Honório <joaquimhonorio@copin.ufcg.edu.br>
2020-07-07 13:17:14 -0300

    Adiciona participantes de uma licitacao

--- src/app/licitacoes/info-licitacao/info-licitacao.component.ts
+++ src/app/licitacoes/info-licitacao/info-licitacao.component.ts
@@ -45,4 +45,17 @@ export class InfoLicitacaoComponent implements OnInit {
     }
   }
 
+  getGanhador (cpfcnpj, contratos){
+    let contemNaLista = false;
+
+    for(let contrato of contratos) {
+      console.log (cpfcnpj == contrato?.nu_cpfcnpj)
+      if (contrato?.nu_cpfcnpj == cpfcnpj) {
+        contemNaLista = true;
+        break;
+      }
+    };
+    return contemNaLista;
+  }
+
 }
