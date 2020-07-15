04275dead6cfcbce0f009567914e21d1895c9bc5
Joaquim Honório <joaquimhonorio@copin.ufcg.edu.br>
2020-07-15 17:18:52 -0300

    Altera formato da listagem

--- src/app/shared/pipes/format-first-letter-capitalize.pipe.ts
+++ src/app/shared/pipes/format-first-letter-capitalize.pipe.ts
@@ -8,6 +8,11 @@ export class FormatFirstLetterCapitalizePipe implements PipeTransform {
   transform(value: string, ...args: any[]): string {
     if (value == '') return
     value = value.toLowerCase();
+
+    if (value.length > 199){
+      value += "..."
+    }
+    
     return value.substring(0,1).toUpperCase()+value.substring(1);
   }
 
