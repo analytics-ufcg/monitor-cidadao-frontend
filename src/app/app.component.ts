29f166a93978426dadebaac2268f7b5a021489c6
diegooalmeida <diego.almeida@ccc.ufcg.edu.br>
2020-09-22 16:58:45 -0300

    Adiciona conexão ao google analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
+import { Router, NavigationEnd } from '@angular/router';
 import { Component } from '@angular/core';
+
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
@@ -8,4 +13,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'client';
 
+  constructor(private router: Router){
+    const navEndEvents$ = this.router.events
+    .pipe(
+      filter(event => event instanceof NavigationEnd)
+    );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'UA-178653936-1', {
+        'page_path': event.urlAfterRedirects
+      });
+    }); 
+  }
 }
